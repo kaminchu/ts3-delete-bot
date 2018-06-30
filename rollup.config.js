26a51a4f87cbaee22d62827d38b8fb6fb8904078
kaminchu <kmchu.mail@gmail.com>
2018-06-30 12:30:50 +0900

    uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
 import babel from "rollup-plugin-babel";
 import babelrc from "babelrc-rollup";
+import { uglify } from "rollup-plugin-uglify";
 
 export default {
   entry: "src/index.js",
   dest: "index.js",
   format: "umd",
-  plugins: [babel(babelrc())]
+  plugins: [
+    babel(babelrc()),
+    uglify()
+  ]
 }
